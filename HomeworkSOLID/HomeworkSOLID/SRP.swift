d5739bc98441c65dac8d0413bae548d4e9c81ffc
Ivan Znak <ivanznak04@gmail.com>
2023-12-14 16:34:03 +0100

    Project was added

---
+++ HomeworkSOLID/HomeworkSOLID/SRP.swift
@@ -0,0 +1,40 @@
+//
+//  SingelResponsibilityPrinciple.swift
+//  HomeworkSOLID
+//
+//  Created by Иван Знак on 14/12/2023.
+//
+
+import Foundation
+// Bad Example 1
+class ChatManager {
+    func takeMessage(){
+        
+    }
+    func sendMessage(){
+        
+    }
+}
+//Good example 1
+class TakeMessage{
+    func takeMessage(){}
+}
+class SendMessage{
+    func sendMessage(){}
+}
+
+//Bad example 2
+class Order {
+    func makeOrder(){
+        
+    }
+    func printSumOfOrder(){}
+}
+
+//Good example 2
+class Order1{
+    func makeOrder(){}
+}
+class OrderPrint{
+    func printOrder(){}
+}
